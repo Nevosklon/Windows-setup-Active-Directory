6490ba309c98ce47695d83fd62e5905a24e3cebc
nevosklon <154698393+Nevosklon@user.noreply.github.com>
2024-05-05 11:31:44 -0700

    Regedit settings

--- Window-Setup.ps1
+++ Window-Setup.ps1
@@ -1,47 +1,70 @@
 
 $RegAdd=[ordered]@{
-	"HKLM:\SOFTWARE\Policies\Microsoft\Windows\Personalization"=@(@{key="NoLockScreen"; type="DWORD"; value=1});
+	"HKLM:\SOFTWARE\Policies\Microsoft\Windows\Personalization"=@(
+		@{name="NoLockScreen"; type="DWORD"; value=1}
+	);
 	"HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"=@(
-		@{key="DisallowShaking"; type="DWORD"; value=1}
+		@{name="DisallowShaking"; type="DWORD"; value=1}
 	);
 	"HKCU:\SOFTWARE\Policies\Microsoft\Windows\Personalization"=@(
-		@{key="AppsUseLightTheme"; type="DWORD"; value=0},
-		@{key="SystemUseLightTheme"; type="DWORD"; value=0}
+		@{name="AppsUseLightTheme"; type="DWORD"; value=0},
+		@{name="SystemUseLightTheme"; type="DWORD"; value=0}
 	);
 	"HKCU:\Control Panel\Desktop"=@(
-		@{key="MenuShowDelay"; type="String"; value=0}
+		@{name="MenuShowDelay"; type="String"; value=0}
 	);
 	"HKCU:\Software\Policies\Microsoft\Windows\Explorer"=@(
-		@{key="DisableSearchBoxSuggestions"; type="DWORD"; value=1}
+		@{name="DisableSearchBoxSuggestions"; type="DWORD"; value=1}
 	);
 	"HKCR:\DesktopBackground\Shell\powershell\command"=@(
-		@{key="(Default)"; type ="String"; value="C:\Windows\System32\WindowsPowerShell\v1.0"}
+		@{name="(Default)"; type ="String"; value="C:\Windows\System32\WindowsPowerShell\v1.0"}
 	);
 	"HKCR:\DesktopBackground\Shell\Display\command"=@(
-		@{key="DelegateExecute"; type ="String"; value="{556FF0D6-A1EE-49E5-9FA4-90AE116AD744}"}
+		@{name="DelegateExecute"; type ="String"; value="{556FF0D6-A1EE-49E5-9FA4-90AE116AD744}"}
 	);	
 }
 
-function Reg_AddKey{
+function Reg-AddKey{
 	param(
 		[Parameter(Mandatory=$true)]
 		[string]$Path
 	)
 	$PathArray = $Path.Split("\")
-	Write-Host  $PathArray
-	$Path.GetType()
-	for( $i = 0; $i -lt $PathArray.Length; $i++) {
-		$tmp = [system.String]::Join("\", $Path[0..$i])
-		#$tmp.GetType()
-		Write-Host "$tmp"
+	Write-Host  $Path
+	#Write-Host  $PathArray
+	for( $i = 1; $i -lt $PathArray.Length; $i++) {
+		$path = [system.String]::Join("\", $PathArray[0..($i - 1)])
+		$key = $PathArray[$i]
+		Write-Host "$path"
+		New-Item -Path $path -Name $key
+		
 	}
 	
 }
-Reg_AddKey "HKCR:\DesktopBackground\Shell\powershell\command"
+
+function Reg-AddItem{
+	param(
+		[Parameter(Mandatory=$true)]
+		[Object]$Items,
+		[String]$path
+	)
+	foreach( $v in $Items) {
+		$type = $v["type"]
+		$name = $v["name"] 
+		$value = ($v["value"])
+		New-ItemProperty -PropertyType $type -Path $path -Name $name -Value $value -Verbose
+		
+		# TODO: check for property exist and modify values
+	}
+
+}
+
+# TODO: reduce error outputs
 foreach($k in $RegAdd.getEnumerator()) {
-	# $v = $RegAdd[$k][0]["key"]
-	$v = ($k.Value)[0]["key"]
+	$v = $k.Value
 	$k = $k.Name
-	#echo $k
-	#Reg_AddKey $k
+
+	Reg-AddKey $k
+	Reg-AddItem $v $k
+
 }
