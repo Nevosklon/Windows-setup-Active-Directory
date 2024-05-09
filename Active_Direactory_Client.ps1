f216b88d82dbe704d544655bfa7194ce31eba522
Nevosklon <154698393+Nevosklon@users.noreply.github.com>
2024-05-10 00:06:30 +0800

    Consistent tabs and spacing
    For some reason windows is messing up the tabs

--- Active_Direactory_Client.ps1
+++ Active_Direactory_Client.ps1
@@ -1,18 +1,17 @@
 function main {
 	$IPAddress = Read-Host "Enter Server IPv4 address"
-    $USER = Read-Host "Enter User Name"
+ 	$USER = Read-Host "Enter User Name"
 
 	$DOMAIN="Barportestates.com"
-    $LOCALDOMAIN = "Barpo"
+ 	$LOCALDOMAIN = "Barpo"
 
-    # Clear Dns and register dns before setting new domain
-    Clear-DnsClientCache -Verbose
-	Get-NetAdapter | Set-DnsClientServerAddress -Addresses "$IPAddress" -Verbose    
-    Register-DnsClient -Verbose
+	# Clear Dns and register dns before setting new domain
+ 	Clear-DnsClientCache -Verbose
+	Get-NetAdapter | Set-DnsClientServerAddress -Addresses "$IPAddress" -Verbose
+ 	Register-DnsClient -Verbose
 
 	Add-Computer -Domain "$DOMAIN" -DomainCredential "$USER@$DOMAIN" -LocalCredential "$LOCALDOMAIN\$USER" -Force
-
-    Read-Host "Press enter To restart pc"
-    Restart-Computer
+ 	Read-Host "Press enter To restart pc"
+	Restart-Computer
 }
 main
