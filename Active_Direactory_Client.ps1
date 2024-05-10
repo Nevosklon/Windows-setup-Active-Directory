1080bcecf9406e4659305bc6ff1953f62b6ebf5d
nevosklon <154698393+Nevosklon@user.noreply.github.com>
2024-05-09 18:37:09 -0700

    Fixing client
    Merging and rebase

--- Active_Direactory_Client.ps1
+++ Active_Direactory_Client.ps1
@@ -1,12 +1,18 @@
 function main {
 	$IPAddress = Read-Host "Enter Server IPv4 address"
-    $USER = Read-Host "Enter User"
-	$DOMAIN="Barportestates.com"
+    $USER = Read-Host "Enter User Name"
 
-	Get-NetAdapter | Set-DnsClientServerAddress -Addresses $IPAddress -Validate -Verbose
-    Rename-Computer $USER
-	Add-Computer -Domain $DOMAIN -Credential "Barpo\$USER" -Force
-	Read-Host "Press any Key to Exit"
-	Restart-Computer
+	$DOMAIN="Barportestates.com"
+    $LOCALDOMAIN = "Barpo"
+
+    # Clear Dns and register dns before setting new domain
+    Clear-DnsClientCache -Verbose
+	Get-NetAdapter | Set-DnsClientServerAddress -Addresses "$IPAddress" -Verbose    
+    Register-DnsClient -Verbose
+
+	Add-Computer -Domain "$DOMAIN" -DomainCredential "$USER@$DOMAIN" -LocalCredential "$LOCALDOMAIN\$USER" -Force
+
+    Read-Host "Press enter To restart pc"
+    Restart-Computer
 }
 main
